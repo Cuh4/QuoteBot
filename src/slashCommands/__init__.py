d4925b459381fe5edc25e993d9e7b808b34165ff
Cuh4 <roozzguy@gmail.com>
2023-11-15 19:14:57 +0000

    forgot to import new command

--- src/slashCommands/__init__.py
+++ src/slashCommands/__init__.py
@@ -5,5 +5,6 @@
 def start():
     # importing the commands will automatically start them
     # is this a good way to do things? probably not
+    from .getRandomQuote import command as _
     from .quote import command as _
     from .restart import command as _
