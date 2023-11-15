b777559adef337a3a019db7cd14af1507865931b
Cuh4 <roozzguy@gmail.com>
2023-11-15 20:24:46 +0000

    new "jump to quote" button

--- src/ui/views/quoteView.py
+++ src/ui/views/quoteView.py
@@ -6,15 +6,35 @@
 import discord
 
 import ui
+import quotes
 from ui.views import template
+from helpers import general as helpers
+from helpers import discord as discordHelpers
 
 # // ---- Main
 # // UI
 class view(template):
     # // Main UI
-    def __init__(self):
+    def __init__(self, quote: quotes.definitions.quote):
         # // setup
+        # setup template
         super().setup()
+        
+        # get vars
+        client: discord.Client = helpers.globals.get("client")
+        message: discord.PartialMessageable = client.get_partial_messageable(quote.getMessageID(), guild_id = quote.getGuildID())
+        
+        # // jump to message button
+        # create button
+        self.jumpToMessage = discord.ui.Button(
+            style = discord.ButtonStyle.blurple,
+            label = "Jump To Quote",
+            url = message.jump_url if message else "",
+            disabled = message is None
+        )
+        
+        # add
+        self.add_item(self.jumpToMessage)
 
         # // discord invite button
         # create button
