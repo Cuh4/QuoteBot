c66502a32834e2185509330aa8e0f3558a4e1f37
Cuh4 <roozzguy@gmail.com>
2023-11-16 00:25:33 +0000

    function instead of subclass or whatever

--- src/ui/embeds/quote.py
+++ src/ui/embeds/quote.py
@@ -13,30 +13,31 @@ from helpers import discord as discordHelpers
 
 # // ---- Main
 # // UI
-class embed(discord.Embed):
-    def __init__(self, quote: quotes.classes.quote):
-        # // get variables
-        # discord related
-        client: discord.Client = helpers.globals.get("client")
-        
-        # quote related
-        quoteText = helpers.misc.truncateIfTooLong(quote.getText(), config.maxQuoteLength) # enforce character limit
-        quoteText = discordHelpers.utils.stripMarkdown(quoteText) # remove markdown
+async def embed(quote: quotes.classes.quote):
+    # // get variables
+    # discord related
+    client: discord.Client = helpers.globals.get("client")
+    
+    # quote related
+    quoteText = helpers.misc.truncateIfTooLong(quote.getText(), config.maxQuoteLength) # enforce character limit
+    quoteText = discordHelpers.utils.stripMarkdown(quoteText) # remove markdown
 
-        # quote author related
-        user = client.get_user(quote.getUserID())
-        name = "Anonymous"
-        avatar_url = client.user.display_avatar.url
+    # quote author related
+    user = client.get_user(quote.getUserID()) or await client.fetch_user(quote.getUserID())
+    name = "Anonymous"
+    avatar_url = client.user.display_avatar.url
 
-        if user is not None:
-            name, avatar_url = f"{user.display_name} (@{user.name})", user.display_avatar.url # get user's name and avatar url
-        
-        # // main
-        # create embed
-        super().__init__(
-            description = f">>> **{quoteText}**",
-            color = discord.Color.from_rgb(*[random.randint(50, 255) for i in range(3)])
-        )
-        
-        self.set_author(name = name, url = discordHelpers.utils.linkUser(user) if user else "", icon_url = avatar_url)
-        self.set_footer(text = f"Quote ID: {quote.getID()} | Channel ID: {quote.getChannelID()}")
+    if user is not None:
+        name, avatar_url = f"{user.display_name} (@{user.name})", user.display_avatar.url # get user's name and avatar url
+    
+    # // main
+    # create embed
+    embed = discord.Embed(
+        description = f">>> **{quoteText}**",
+        color = discord.Color.from_rgb(*[random.randint(50, 255) for i in range(3)])
+    )
+    
+    self.set_author(name = name, url = discordHelpers.utils.linkUser(user) if user else "", icon_url = avatar_url)
+    self.set_footer(text = f"Quote ID: {quote.getID()} | Channel ID: {quote.getChannelID()}")
+    
+    return embed
